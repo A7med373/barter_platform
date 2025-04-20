7455b59c900dba55b8f425841b5d2bd0608e1218
A7med373 <a7medsaif2005@gmail.com>
2025-04-20 14:00:07 +0300

    Реализовал функционал с обменами

--- apps/ads/apps.py
+++ apps/ads/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AdsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ads'
+    verbose_name = 'Объявления'
+
+    def ready(self):
+        # Import template tags to register them
+        from . import templatetags
--- apps/ads/views_html.py
+++ apps/ads/views_html.py
@@ -1,9 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db import IntegrityError
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 
-from .forms import AdForm, ProposalForm
+from .forms import AdForm
+from .forms import ProposalForm
 from .models import Ad
 
 
@@ -21,9 +26,12 @@ class AdListView(ListView):
         category = self.request.GET.get("category")
         condition = self.request.GET.get("condition")
         if q:
-            qs = qs.filter(Q(title__icontains=q) | Q(description__icontains=q))
+            qs = qs.filter(
+                Q(title__icontains=q) |
+                Q(description__icontains=q)
+            )
         if category:
-            qs = qs.filter(category=category)
+            qs = qs.filter(category__icontains=category)
         if condition:
             qs = qs.filter(condition=condition)
         return qs
@@ -34,12 +42,18 @@ class AdDetailView(DetailView):
     template_name = "ads/ad_detail.html"
     context_object_name = "ad"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated and self.request.user != self.object.user:
+            context['user_ads'] = Ad.objects.filter(user=self.request.user)
+        return context
+
 
 class AdCreateView(LoginRequiredMixin, CreateView):
     model = Ad
     form_class = AdForm
     template_name = "ads/ad_form.html"
-    success_url =  reverse_lazy("ads:ad_list")
+    success_url = reverse_lazy("ads:ad_list")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -50,7 +64,7 @@ class AdUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Ad
     form_class = AdForm
     template_name = "ads/ad_form.html"
-    success_url =  reverse_lazy("ads:ad_list")
+    success_url = reverse_lazy("ads:ad_list")
 
     def test_func(self):
         return self.get_object().user == self.request.user
@@ -59,7 +73,7 @@ class AdUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class AdDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Ad
     template_name = "ads/confirm_delete.html"
-    success_url =  reverse_lazy("ads:ad_list")
+    success_url = reverse_lazy("ads:ad_list")
 
     def test_func(self):
         return self.get_object().user == self.request.user
@@ -70,8 +84,34 @@ class AdDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 class ProposalCreateView(LoginRequiredMixin, CreateView):
     form_class = ProposalForm
     template_name = "ads/proposal_form.html"
-    success_url =  reverse_lazy("ads:ad_list")
+    success_url = reverse_lazy("ads:ad_list")
+
+    def get_ad(self):
+        return get_object_or_404(Ad, pk=self.kwargs["ad_id"])
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["ad_sender"] = self.get_ad()
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["ad"] = self.get_ad()
+        return ctx
 
     def form_valid(self, form):
-        form.instance.ad_sender_id = self.kwargs["ad_id"]
-        return super().form_valid(form)
+        ad_sender = self.get_ad()
+        form.instance.ad_sender = ad_sender
+        try:
+            self.object = form.save()
+            messages.success(self.request, "Предложение отправлено.")
+            return redirect("ads:ad_detail", pk=ad_sender.pk)
+        except IntegrityError:
+            messages.error(self.request, "Вы уже отправляли такое предложение.")
+            return redirect("ads:ad_detail", pk=ad_sender.pk)
+
+    def form_invalid(self, form):
+        ad_sender = self.get_ad()
+        for err in form.non_field_errors():
+            messages.error(self.request, err)
+        return redirect("ads:ad_detail", pk=ad_sender.pk)
